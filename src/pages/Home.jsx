d4c38a58b8b80240bebbc172ff41e3e0930dd1ce
mrsonukr <MSSONUKR@GMAIL.COM>
2025-05-03 21:13:38 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,19 @@ const Home = () => {
         <ListCard />
         <ContactCard />
       </div>
+      <div className=" h-20 mt-14 px-16 flex items-center justify-between">
+        <div>
+          <p className="uppercase text-lg font-semibold">Our Services</p>
+          <p className="text-3xl mt-1 text-green-500 font-bold">
+            Tailored Solutions According to Your Needs
+          </p>
+        </div>
+        <div>
+          <button className="uppercase bg-orange-500 py-3 px-6 text-white rounded-xl hover:bg-orange-600 transition duration-200 text-sm sm:text-base">
+            Book An Appointment
+          </button>
+        </div>
+      </div>
       <Footer />
     </div>
   );
